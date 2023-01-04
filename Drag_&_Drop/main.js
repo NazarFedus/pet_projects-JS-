c0fdf239c68a3a5cdd3b0879b785575dd4285c35
NazarFedus <simpleprograming7@gmail.com>
2023-01-04 15:51:42 +0000

    add project

---
+++ Drag_&_Drop/main.js
@@ -0,0 +1,30 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+// when we start to move the item
+item.addEventListener('dragstart', (event) => {
+     event.target.classList.add('hold')
+     setTimeout(() => event.target.classList.add('hide'), 0)
+})
+// when we finish to move the item
+item.addEventListener('dragend', (event) => {
+     event.target.className = 'item'
+})
+
+placeholders.forEach(placeholder => {
+     // to remove the default behavior of the browser
+     placeholder.addEventListener('dragover', (event) => {
+          event.preventDefault();
+     })
+     placeholder.addEventListener('dragenter', (event) => {
+          event.target.classList.add('hovered');
+     })
+     placeholder.addEventListener('dragleave', (event) => {
+          event.target.classList.remove('hovered');
+     })
+     // add item to placeholder
+     placeholder.addEventListener('drop', (event) => {
+          event.target.classList.remove('hovered');
+          event.target.append(item);
+     })
+})
